8dcb72135a3cc9d1fe8b10702c377ea9ab04843f
gitttf <hun.a@wp.pl>
2019-08-20 16:48:27 +0200

    removed load

--- js/pages.js
+++ js/pages.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	// default
-	$("#sidemenu").load("html/sidemenu.html .one");
-	$("#contentBox").load("html/default.html #content_default");
+	//$("#sidemenu").load("html/sidemenu.html .one");
+	//$("#contentBox").load("html/default.html #content_default");
 });
