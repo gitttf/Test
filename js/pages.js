b71e1aa3a42f937998dcd3cc6e08fa06e33fd564
gitttf <hun.a@wp.pl>
2019-08-20 17:10:45 +0200

    pronto event

--- js/pages.js
+++ js/pages.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
+$(window).on('pronto.request', function() {
 	// default
-	//$("#sidemenu").load("html/sidemenu.html .one");
-	//$("#contentBox").load("html/default.html #content_default");
+	$("#sidemenu").load("html/sidemenu.html .one");
+	$("#contentBox").load("html/default.html #content_default");
 });
